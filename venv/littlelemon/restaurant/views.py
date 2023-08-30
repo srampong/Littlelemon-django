48ec541d718b5df329f943757eb3ac7c0250d28c
srampong <ampong.stephen.rexford>
2023-08-30 11:28:59 +0000

    Booking API

--- venv/littlelemon/restaurant/views.py
+++ venv/littlelemon/restaurant/views.py
@@ -1,4 +1,25 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import Menu,Booking
+from rest_framework.decorators import api_view
+from .serializers import MenuItemSerializer,BookingSerializer
+from rest_framework import viewsets
+
+class MenuItemsView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuItemSerializer
+  
+class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+      queryset = Menu.objects.all()
+      serializer_class = MenuItemSerializer
+
+
+class BookingViewSet(viewsets.ModelViewSet):
+      queryset = Booking.objects.all()
+      serializer_class = BookingSerializer
+      #permission_classes = [permissions.IsAuthenticated] 
+  
+  
 
 # Create your views here.
 def index(request):
